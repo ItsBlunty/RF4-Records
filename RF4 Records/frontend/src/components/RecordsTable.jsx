a02da07d8942b06fba9862d24ca03e720eb954d3
itsblunty <itsbluntyfpv@gmail.com>
2025-06-28 17:26:28 -0500

    Make trophy icons narrower - reduce width back to w-2 while keeping h-8 height

--- RF4 Records/frontend/src/components/RecordsTable.jsx
+++ RF4 Records/frontend/src/components/RecordsTable.jsx
@@ -10,12 +10,12 @@ const RecordsTable = ({ records, sortConfig, onSort }) => {
 
   const getTrophyIcon = (trophyClass) => {
     if (trophyClass === 'record') {
-      return <img src={superTrophyIcon} alt="Record" className="w-3 h-8 mr-1 inline-block" />;
+      return <img src={superTrophyIcon} alt="Record" className="w-2 h-8 mr-1 inline-block" />;
     } else if (trophyClass === 'trophy') {
-      return <img src={trophyIcon} alt="Trophy" className="w-3 h-8 mr-1 inline-block" />;
+      return <img src={trophyIcon} alt="Trophy" className="w-2 h-8 mr-1 inline-block" />;
     }
     // Return spacer element to maintain alignment - match the width of actual trophies
-    return <span className="w-3 h-8 mr-1 inline-block"></span>;
+    return <span className="w-2 h-8 mr-1 inline-block"></span>;
   };
 
   const getSortIndicator = (columnKey) => {
