76c18e8891a520897b1c104ea2f77cd4ea9927cf
itsblunty <itsbluntyfpv@gmail.com>
2025-07-04 11:19:35 -0500

    Reorder maps: Elk after Mosquito, Copper after Akhtuba

--- RF4 Records/frontend/src/config/maps.js
+++ RF4 Records/frontend/src/config/maps.js
@@ -1,9 +1,8 @@
 // Available maps configuration
 // Map from URL name to filename
 export const availableMaps = {
-  'elklake': 'Elk Lake_38_38_162_161.5.png',
-  'copper': 'Copper Lake_24_23.5_78_77.png',
   'mosquito': 'Mosquito Lake_30_33_112_115.5.png',
+  'elklake': 'Elk Lake_38_38_162_161.5.png',
   'winding': 'Winding Rivulet_45_54_141.5_150.5.png',
   'oldburg': 'Old Burg_-2_-2_83_83.png',
   'belaya': 'Belaya River_-1_-9_114_107.png',
@@ -16,5 +15,6 @@ export const availableMaps = {
   'amber': 'Amber Lake_-2_18_194_212.5.png',
   'archipelago': 'Ladoga Archipelago_-25.5_-26_625_625.png',
   'akhtuba': 'Akhtuba River_-6_-8.5_207_208.png',
+  'copper': 'Copper Lake_24_23.5_78_77.png',
   'lowertunguska': 'Lower Tunguska River_-10.5_-10.5_251_251.png'
 };
